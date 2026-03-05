f141a95c7c81f8a05f1b3e6e7ed2ab6d245f0856
Sky Shaver <skyshaver@me.com>
2026-03-04 16:46:26 -0800

    setup testing

---
+++ test/source/JsonSerializerTest.cpp
@@ -0,0 +1,89 @@
+#include <sky_trem_plugin/sky_trem_plugin.h>
+#include <gtest/gtest.h>
+
+namespace sky_trem {
+TEST(JsonSerializer, SerializeToString) {
+  PluginProcessor processor;
+  auto& parameters = processor.getParameterRefs();
+
+  parameters.modulationRate = 10.f;
+  parameters.bypass = true;
+  parameters.lfoWaveform = 1;
+
+  const juce::String expectedOutput =
+      u8R"({
+  "__version__": 1,
+  "pluginName": "SkyTrem",
+  "modulationRate": 10.0,
+  "gainInDb": 0.0,
+  "bypass": true,
+  "lfoWaveform": "Triangle"
+})";
+  juce::MemoryBlock block;
+  juce::MemoryOutputStream outputStream{block, false};
+
+  JsonSerializer::serialize(parameters, outputStream);
+  outputStream.flush();
+
+  const auto result = outputStream.toUTF8().removeCharacters("\r");
+
+  EXPECT_EQ(expectedOutput, result);
+}
+
+TEST(JsonSerializer, DeserializeFromString) {
+  const juce::String savedParameters =
+      u8R"({
+  "__version__": 1,
+  "pluginName": "SkyTrem",
+  "modulationRate": 10.0,
+  "gainInDb": 0.0,
+  "bypass": true,
+  "lfoWaveform": "Triangle"
+})";
+
+  juce::MemoryInputStream inputStream{
+      savedParameters.getCharPointer(),
+      static_cast<size_t>(savedParameters.length()), false};
+
+  PluginProcessor processor;
+  auto& parameters = processor.getParameterRefs();
+
+  const auto result = JsonSerializer::deserialize(inputStream, parameters);
+
+  EXPECT_TRUE(result.wasOk());
+  EXPECT_FLOAT_EQ(parameters.modulationRate, 10.f);
+  EXPECT_TRUE(parameters.bypass);
+  EXPECT_EQ(juce::String{"Triangle"},
+            parameters.lfoWaveform.getCurrentChoiceName());
+}
+
+TEST(JsonSerializer, DontUpdateParametersWhenWaveformNameIsInvalid) {
+  // given
+  const juce::String savedParameters =
+      u8R"({
+  "__version__": 1,
+  "pluginName": "SkyTrem",
+  "modulationRate": 10.0,
+  "gainInDb": 0.0,
+  "bypass": true,
+  "lfoWaveform": "Triangle"
+})";
+
+    juce::MemoryInputStream inputStream{ savedParameters.getCharPointer(), static_cast<size_t>(savedParameters.length()), false};
+
+  PluginProcessor processor;
+  auto& parameters = processor.getParameterRefs();
+  parameters.lfoWaveform = 0;
+  parameters.bypass = false;
+  parameters.modulationRate= 5.f;
+
+  // when
+  const auto result = JsonSerializer::deserialize(inputStream, parameters);
+
+  // then
+  EXPECT_TRUE(result.failed());
+  EXPECT_FLOAT_EQ(parameters.modulationRate.get(), 5.f);
+  EXPECT_FALSE(parameters.bypass.get());
+  EXPECT_EQ(0, parameters.lfoWaveform.getIndex());
+}
+}  // namespace sky_trem
