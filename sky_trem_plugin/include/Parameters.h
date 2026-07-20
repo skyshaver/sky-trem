59033b292884e1d2e964d8b6b60780ca1c389a8f
Sky Shaver <skyshaver@me.com>
2026-07-20 16:20:26 -0700

    start writing parameter parsing/gen

--- sky_trem_plugin/include/Parameters.h
+++ sky_trem_plugin/include/Parameters.h
@@ -5,19 +5,19 @@ namespace sky_trem {
 	struct Parameters {
 
 		explicit Parameters(juce::AudioProcessor&);
+
+		//--start parameters
 		juce::AudioParameterFloat& modulationRate;
 		juce::AudioParameterFloat& modulationDepth;
 		juce::AudioParameterFloat& gainInDb;
 		juce::AudioParameterBool& bypass;
 		juce::AudioParameterChoice& lfoWaveform;
-		juce::AudioParameterChoice& bpmDivision;
-		// represents the state of Rate in HZ or Bpm Division for calculating modRate
+		juce::AudioParameterChoice& bpmDivision;		
 		juce::AudioParameterBool& isRateInHz;
 		juce::AudioParameterFloat& bpm;
-
 		juce::AudioParameterBool& isModDepthRando;
 		juce::AudioParameterChoice& modDepthRandoRange;
-
+		//--stop parameters
 		static constexpr int versionHint = 1;
 
 		JUCE_DECLARE_NON_COPYABLE(Parameters);
